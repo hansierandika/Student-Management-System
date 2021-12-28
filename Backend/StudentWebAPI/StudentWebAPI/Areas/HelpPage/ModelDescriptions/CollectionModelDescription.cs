f28df3838af0be575a0e1cc1de847c6e9f437417
hansierandika <erandikahansi95@gmail.com>
2021-12-28 23:32:39 +0530

    backend changed

--- Backend/StudentWebAPI/StudentWebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace StudentWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Backend/StudentWebAPI/StudentWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace StudentWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
