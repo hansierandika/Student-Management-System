e978bec7833b5b2a0e076afb156764318b618546
hansierandika <erandikahansi95@gmail.com>
2022-11-03 00:18:15 +0530

    Student Registraion No Dropdown

--- Frontend/studentM/src/app/add-edit-mark/add-edit-mark.component.ts
+++ Frontend/studentM/src/app/add-edit-mark/add-edit-mark.component.ts
@@ -1,3 +1,7 @@
+import { Mark } from './../models/mark';
+import { student } from './../models/studentData';
+import { DetailServiceService } from './../detail-service.service';
+import { Student } from './../models/student';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditMarkComponent implements OnInit {
 
-  constructor() { }
+  studentNo:string
+  students:Student[]
+  mark:Mark
+
+  constructor(private detailService: DetailServiceService) {
+    this.detailService.getStudents().subscribe((students: Student[]) => this.students = students);
+   }
 
   ngOnInit(): void {
+    this.mark=new Mark();
   }
 
+  public loadStudentNo(e:any): void {
+    let find = this.students.find(x => x?.RegistrationNo === e.target.value);
+    console.log(find?.RegistrationNo);
+  }
 }
